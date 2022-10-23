7a5b351563086a134ea283d85627cbdbfa9e60fc
ElifBC <ebilgeca@my.centennialcollege.ca>
2022-10-23 18:17:02 -0400

    authentication completed

--- app/controllers/movies.controller.server.js
+++ app/controllers/movies.controller.server.js
@@ -1,4 +1,5 @@
 import movieModel from '../models/movies.js';
+import {UserDisplayName } from "../utils/index.js";
 
 export function DisplayMovieList(req, res, next){
     movieModel.find(function(err, moviesCollection) {
@@ -7,13 +8,13 @@ export function DisplayMovieList(req, res, next){
             res.end(err);
         }
 
-        res.render('index', {title: 'Movie List', page: 'movies/list', movies: moviesCollection});
+        res.render('index', {title: 'Movie List', page: 'movies/list', movies: moviesCollection, displayName: UserDisplayName(req) });
 
     })
 }
 
 export function DisplayMovieAddPage(req, res, next){
-    res.render('index', { title: 'Add Movie', page: 'movies/edit', movie: {}});
+    res.render('index', { title: 'Add Movie', page: 'movies/edit', movie: {}, displayName: UserDisplayName(req)});
 }
  
 export function ProcessMovieAddPage(req, res, next){
@@ -43,7 +44,7 @@ export function DisplayMovieEditPage(req, res, next){
             res.ens(err);
         }
 
-        res.render('index', { title: 'Edit Movie', page: 'movies/edit', movie: movie });
+        res.render('index', { title: 'Edit Movie', page: 'movies/edit', movie: movie, displayName: UserDisplayName(req) });
     })
    
 }
