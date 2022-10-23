b19fefc2f38e9ed704438ae66a07b6b4702a9b02
ElifBC <ebilgeca@my.centennialcollege.ca>
2022-10-23 17:29:36 -0400

    Authentication completed

--- app/controllers/auth.controller.server.js
+++ app/controllers/auth.controller.server.js
@@ -49,3 +49,43 @@ export function ProcessLoginPage(req, res, next){
         
     })(req, res, next);
 }
+
+export function ProcessRegisterPage(req, res, next){
+    let newUser = new User({
+        username: req.body.username,
+        emailAddress: req.body.emailAddress,
+        displayName: req.body.firstName + " " + req.body.lastName
+    });
+
+    User.register(newUser, req.body.password, function(err){
+        if(err){
+            if(err.name == "UserExistsError"){
+                console.error('ERROR: User Already Exists!');
+                req.flash('registerMessage', 'Registration Error')
+            } else {
+                console.error(err.name);
+                req.flash('registerMessage', 'Server Error')
+            }
+            
+            return res.redirect('/register');
+        }
+
+        return passport.authenticate('local')(req, res, function()
+        {
+            return res.redirect('/');
+        });
+    });
+}
+
+export function ProcessLogoutPage(req, res, next){
+    req.logOut(function(err){
+        if(err){
+            console.error(err);
+            res.end(err);
+        }
+
+        console.log("user logged out successfully");
+    });
+
+    res.redirect('/login');
+}
---
+++ app/routes/auth.route.server.js
@@ -0,0 +1,20 @@
+import { Router} from "express";
+import { DisplayLoginPage, DisplayRegisterPage, ProcessLoginPage, ProcessLogoutPage, ProcessRegisterPage } from "../controllers/auth.controller.server.js";
+
+const router = Router();
+
+//Display Login Router
+router.get('/login', DisplayLoginPage);
+// Process Login Page
+router.post('/login', ProcessLoginPage);
+
+//display Register router
+router.get('/register', DisplayRegisterPage);
+
+// Process Registration Page
+router.post('/register', ProcessRegisterPage);
+
+// Process lougout 
+router.get('/logout', ProcessLogoutPage);
+
+export default router;
