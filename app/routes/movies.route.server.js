a784fe30fa6fb608c71e90f2eae93a641eb24806
ElifBC <ebilgeca@my.centennialcollege.ca>
2022-10-23 18:34:26 -0400

    Authorization completed

--- app/routes/movies.route.server.js
+++ app/routes/movies.route.server.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 
 import { DisplayMovieList, DisplayMovieAddPage, ProcessMovieAddPage, ProcessMovieEditPage, DisplayMovieEditPage, ProcessMovieDelete } from "../controllers/movies.controller.server.js";
+import { AuthGuard } from "../utils/index.js";
 
 const router = Router();
 
 router.get('/list', DisplayMovieList);
-router.get('/movie-add', DisplayMovieAddPage);
-router.post('/movie-add', ProcessMovieAddPage);
-router.post('/movie-edit/:id', ProcessMovieEditPage);
-router.get('/movie-edit/:id', DisplayMovieEditPage);
-router.get('/movie-delete/:id', ProcessMovieDelete);
+router.get('/movie-add', AuthGuard, DisplayMovieAddPage);
+router.post('/movie-add', AuthGuard, ProcessMovieAddPage);
+router.post('/movie-edit/:id', AuthGuard, ProcessMovieEditPage);
+router.get('/movie-edit/:id', AuthGuard, DisplayMovieEditPage);
+router.get('/movie-delete/:id', AuthGuard,ProcessMovieDelete);
 
 export default router;
