ea7b047417d9b52398d8a8995c42a7f0e22efcf8
ElifBC <ebilgeca@my.centennialcollege.ca>
2022-10-23 14:18:40 -0400

    Mongo Atlas Setup Completed

--- config/config.js
+++ config/config.js
@@ -1,2 +1,3 @@
 export const Secret = "MySecret";
-export const MongoURI = "mongodb://127.0.0.1:27017/media" // mongo connection URL
+//export const MongoURI = "mongodb://127.0.0.1:27017/media" // mongo connection URL
+export const MongoURI = "mongodb+srv://ElifBC:DartyZigot@mongocluster1.xxdcmfh.mongodb.net/media?retryWrites=true&w=majority"
